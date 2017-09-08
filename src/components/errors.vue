9c1c2a426bad18719897a1ec62b4e11c0c1c8334
Mark Parson <mark@nanobox.io>
2017-09-08 08:52:54 -0600

    Minor update to error styles

--- src/components/errors.vue
+++ src/components/errors.vue
@@ -29,7 +29,7 @@ export default {
 -->
 
 <style lang="scss" scoped>
-  .errors             {width:100%;  border: 4px solid #ED596F; color:#5B3C40; padding:10px; background: white; font-size:16px; z-index: 2;
+  .errors             {width:100%;  border: 4px solid #ED596F; color:#5B3C40; padding:10px; background: white; font-size:16px; z-index: 2; position: relative; margin-bottom:15px; 
     &.hidden          {display: none; }
     &:after           {top: 100%; right: 45px; border: solid transparent; content: ""; height: 0; width: 0; position: absolute; pointer-events: none; border-color: rgba(#ED596F, 0); border-top-color: #ED596F; border-width: 15px; margin-left: -15px;}
   }
