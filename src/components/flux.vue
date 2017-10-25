4f11c3ceaaf3c56702b6e989401bef23d1b784ad
Mark Parson <mark@nanobox.io>
2017-10-25 14:34:24 -0600

    Added transition component

---
+++ src/components/flux.vue
@@ -0,0 +1,44 @@
+<script type="text/babel">
+export default {
+  name: 'flux',
+  props:{
+    transition : {default:"grow", kind:String}
+  },
+  methods:{
+    beforeLeave(el) {
+      // In case this element is deriving its width from its
+      // outer container, lock its width to the current width
+      // before we transition it out as positioned absolute
+      el.style.width = `${el.offsetWidth}px`
+    },
+  }
+}
+</script>
+
+<!--
+  **** H T M L ****
+-->
+
+<template lang="pug">
+  transition-group.item(:name="transition" @before-leave="beforeLeave" )
+    slot
+</template>
+
+<!--
+  ***** C S S *****
+-->
+
+<style lang="scss" >
+  .item > * {
+    transition:all 0.35s cubic-bezier(0.86, 0, 0.07, 1);
+  }
+
+  // Slide / Fade
+  .grow-enter, .grow-leave-to,
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  .grow-leave-active {
+    position: absolute;
+  }
+</style>
